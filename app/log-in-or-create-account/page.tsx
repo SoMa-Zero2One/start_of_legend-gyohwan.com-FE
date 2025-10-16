c82dc177a631d4712b3255b94512572858896737
yoonc01 <yoonc01@hotmail.com>
2025-10-16 18:12:10 +0900

    feat: log-in-or-create-account width-330px로 설정

--- app/log-in-or-create-account/page.tsx
+++ app/log-in-or-create-account/page.tsx
@@ -8,9 +8,9 @@ export default function LoginOrCreateAccount() {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
-      <div className="flex-1 flex flex-col items-center justify-between pt-[60px] pb-[36px] px-[20px]">
-        <div className="w-full space-y-6 flex flex-col items-center">
-          <div className="w-[360px] flex flex-col justify-center gap-[60px]">
+      <div className="flex-1 flex flex-col items-center justify-between pt-[60px] pb-[36px]">
+        <div className="w-full space-y-6 flex flex-col items-center px-[10px]">
+          <div className="w-[330px] flex flex-col justify-center gap-[60px]">
             {/* 헤더 */}
             <div className="text-center">
               <h1 className="text-[24px] font-bold">
