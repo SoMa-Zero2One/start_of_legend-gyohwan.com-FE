8cf0cddadbfd92e12edb94431014a28859489273
yoonc01 <yoonc01@hotmail.com>
2025-10-19 23:09:09 +0900

    feat: Home의 코드 분리

---
+++ components/home/InfoBox.tsx
@@ -0,0 +1,11 @@
+export default function InfoBox() {
+  return (
+    <div className="relative border border-[#056DFF] text-[#056DFF] rounded-[10px] p-[16px] w-fit">
+      <span className="absolute -top-3 left-[12px] bg-white font-bold text-[14px] px-[8px] text-sm">
+        Point!
+      </span>
+      개인 정보는 공개되지 않으며,<br />
+      <span>공유용 닉네임이 자동으로 생성돼요!</span>
+    </div>
+  );
+}
