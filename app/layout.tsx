27dddcecab12c6778dd6fc48436f6598bb9afaa8
yoonc01 <yoonc01@hotmail.com>
2025-10-20 17:56:59 +0900

    chore: 스타일 가이드 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "교환닷컴",
@@ -35,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body
-        className={`m-auto max-w-[455px] ${geistSans.variable} ${geistMono.variable} antialiased`}
+        className="m-auto max-w-[455px] font-pretendard antialiased"
       >
         <div className="flex flex-col">
         {children}
