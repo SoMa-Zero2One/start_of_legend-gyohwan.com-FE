721281db85003126402ced8c89dd7c38471c131d
yoonc01 <yoonc01@hotmail.com>
2025-10-23 11:33:24 +0900

    feat: 짜잘한 오류 수정 - 약관 css 변경 - .gitignore 내용 추가 - 불필요한 import 제거

--- components/auth/TermsAgreement.tsx
+++ components/auth/TermsAgreement.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 export default function TermsAgreement() {
   return (
-    <div className="flex items-center justify-center gap-[20px] text-center text-xs text-gray-500">
-      <Link href="/terms" className="underline hover:text-gray-700">
+    <div className="medium-body-3 flex items-center justify-center gap-[20px] text-center">
+      <Link href="/terms" className="underline">
         이용약관
       </Link>
-      <Link href="/privacy" className="underline hover:text-gray-700">
+      <Link href="/privacy" className="underline">
         개인정보처리방침
       </Link>
     </div>
