d41b64d7a0e65a8ae22828e5ab5832744f17e6b0
yoonc01 <yoonc01@hotmail.com>
2025-11-02 01:20:37 +0900

    feat: main과 merge하기 위해 수정

--- components/home/HeroSection.tsx
+++ components/home/HeroSection.tsx
@@ -2,17 +2,15 @@ import Image from "next/image";
 
 export default function HeroSection() {
   return (
-    <div className="px-[20px]">
-      <div className="relative aspect-[390/360] w-full overflow-hidden rounded-[10px]">
-        <Image src="/images/mainPage-1.png" alt="교환닷컴" fill priority />
-        <div className="absolute inset-0 flex flex-col justify-center gap-[10px] text-center text-white">
-          <p className="g-head-3">
-            준비부터 파견까지,
-            <br />
-            교환학생이 쉬워지는 동행
-          </p>
-          <p className="g-subhead-3">함께하는 길잡이 교환닷컴</p>
-        </div>
+    <div className="relative aspect-[390/360] w-full">
+      <Image src="/images/mainPage-1.png" alt="교환닷컴" fill className="object-cover" priority />
+      <div className="absolute inset-0 flex flex-col justify-center gap-[10px] text-center text-white">
+        <h1 className="g-head-3">
+          준비부터 파견까지,
+          <br />
+          교환학생이 쉬워지는 동행
+        </h1>
+        <p className="g-subhead-3">함께하는 길잡이 교환닷컴</p>
       </div>
     </div>
   );
