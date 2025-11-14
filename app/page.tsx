3453cc31ead8de16bb041d4c7c3d47585bf3255f
yoonc01 <yoonc01@hotmail.com>
2025-11-14 14:06:12 +0900

    feat: remove caching

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,6 @@ import { getSeasons } from "@/lib/api/season";
 import HomePage from "@/components/home/HomePage";
 import StructuredData from "@/components/common/StructuredData";
 
-// 24시간(86400초)마다 자동 재생성
-export const revalidate = 86400;
-
 export const metadata: Metadata = {
   alternates: {
     canonical: "/",
